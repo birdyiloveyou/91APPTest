b0d5ef06a4952a0b708706d5f2dd598fd6dc6f36
ChuChu Chen <birdyiloveyou@gmail.com>
2020-06-21 15:11:39 +0800

    CodeMaid clean up

--- 91APPTest/Controllers/HomeController.cs
+++ 91APPTest/Controllers/HomeController.cs
@@ -1,53 +1,42 @@
 ﻿using _91APPTest.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace _91APPTest.Controllers
 {
     public class HomeController : Controller
     {
-        TestDbEntities db = new TestDbEntities();
-
+        private TestDbEntities db = new TestDbEntities();
 
         public ActionResult Index()
         {
-            
-
-
-            return View(db.OrderList.OrderBy(x=>x.orderItem));
+            return View(db.OrderList.OrderBy(x => x.orderItem));
         }
 
-
-        public ActionResult Detailed() {
-
-            
+        public ActionResult Detailed()
+        {
             return View();
         }
 
-
         [HttpPost]
-        public ActionResult saveChange(List<string> list) {
-
-            foreach (var x in list) {
+        public ActionResult saveChange(List<string> list)
+        {
+            foreach (var x in list)
+            {
                 var change = db.OrderList.Where(id => id.orderId == x).First();
-                change.status = 2;}
+                change.status = 2;
+            }
             db.SaveChanges();
             return null;
         }
 
         [HttpPost]
-        public JsonResult getDetailedData(string data) {
+        public JsonResult getDetailedData(string data)
+        {
             var detailed = db.ItemDetail.Where(x => x.name == data).First();
 
             return Json(detailed);
         }
-
-        
-
-
-
     }
 }
